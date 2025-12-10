f69c423de49ca92f952b66b72ba54ad1f35511a2
Enzo-Campos <93542684+Enzo-Campos@users.noreply.github.com>
2025-12-10 18:53:56 -0300

    Change WhatsApp float link

--- components/FloatWpp.tsx
+++ components/FloatWpp.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 export default function WhatsApp (){
     return (
         <a
-  href="https://api.whatsapp.com/send?phone=5513981339217"
+  href="https://api.whatsapp.com/send/?phone=5513981339217&text=Olá%21+Gostaria+de+agendar+uma+reunião&type=phone_number&app_absent=0"
   target="_blank"
   rel="noopener noreferrer"
   className="fixed bottom-5 right-5 z-[9999] flex h-14 w-14 items-center justify-center rounded-full bg-green-500 text-2xl text-white shadow-lg hover:bg-green-600 transition"
